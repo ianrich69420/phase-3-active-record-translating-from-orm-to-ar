0d3446773e53a64f1450a0f7fb80cb005d59fa6b
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-02 19:59:58 +0300

    commit tree 664ed72cd4288cc7643d204cf896ddf59e8f8869

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,25 @@
-class Dog
+require_relative '../config/environment'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/students.sqlite"
+)
+
+# Create a Dogs table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the dog class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
 end
+
+binding.pry
+""
