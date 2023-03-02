2f57796f5de12dce0fb7b0622525032c2fa178a9
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-03-02 20:08:23 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,26 @@
-class Dog
+require_relative '../config/environment'
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dogs.sqlite"
+)
+
+# Create a Dogs table
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the dog class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
 end
+
+binding.pry
+""
